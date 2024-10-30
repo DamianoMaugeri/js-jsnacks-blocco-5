0df0033cbfcc1bc45fb752b37b55d0f7f6d8bb55
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-10-30 14:35:17 +0100

    [add] snack

---
+++ snack-1/script.js
@@ -0,0 +1,14 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+//1
+console.log(names.join())
+
+//2
+names.forEach((name) => console.log(name))
+
+//3
+for (const name of names) console.log(name)
+
+
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
---
+++ snack-3/script.js
@@ -0,0 +1,7 @@
+const numbers = [2, 8, 4, 7, 2, 87];
+
+// Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+// Risultato: [3, 9, 5, 8, 3, 88]
+
+//numbers.map((num) => num++)
+console.log(numbers.map((num) => num += 1))
